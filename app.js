3f26b12731c936a59f5c7ec38cb9cbb6be1ac69e
zhufengnodejs <zhufengnodejs@126.com>
2016-07-09 17:15:43 +0800

    文件分析

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var app = express();
 //设置模板的存放目录
 app.set('views', path.join(__dirname, 'views'));
 //设置模板引擎
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('.html',require('ejs').__express);
 
 // uncomment after placing your favicon in /public
 //当你在public目录下放置了收藏夹图标后可以取消此注释
@@ -30,20 +31,27 @@ app.use(cookieParser());//处理cookie
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);//指定路由
-app.use('/users', users);
+app.use('/users', users);//指定用户路由
 
 // catch 404 and forward to error handler
+// 捕获404错误并转向错误处理中间件
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  var err = new Error('Not Found');//构建ERROR对象
+  err.status = 404;//设置响应码
+  next(err);//向下传递
 });
 
 // error handlers
-
+//错误处理
 // development error handler
 // will print stacktrace
+//开发错误处理的时候将要打印堆栈信息
+//env是通过express读取环境变量中的NODE_ENV变量来设置到app 中去的
+console.log('env',app.get('env'));
+// product
 if (app.get('env') === 'development') {
+  // 通过中间件函数的参数数量来判断什么是错误处理中间件
+  //如果有4个参数就是错误处理中间件
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -55,6 +63,8 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
+//生产环境 线上正式环境 错误处理
+//不向用户暴露堆栈信息
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
 
 module.exports = router;
